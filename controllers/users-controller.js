bb466c36c4ac5bf3a9a688d592127706c048ec56
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-10-06 19:36:09 -0400

    updated user controller to properly pull id, delete and update.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -3,36 +3,24 @@ const { User } = require('../models');
 const userController = {
   // get all users
   getAllUser(req, res) {
-    User.find({})
-      .populate({
-        path: 'thoughts',
-        select: '-__v'
-      })
-      .select('-__v')
-      .sort({ _id: -1 })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
   },
 
   // get one user by id
-  getUserById({ params }, res) {
-    User.findOne({ _id: params.id })
-      .populate({
-        path: 'thoughts',
-        select: '-__v'
-      })
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.userId })
       .select('-__v')
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
   },
 
-  // createUser
+  // create a new user
   createUser({ body }, res) {
     User.create(body)
       .then(dbUserData => res.json(dbUserData))
@@ -41,7 +29,7 @@ const userController = {
 
   // update user by id
   updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, {
+    User.findOneAndUpdate({ _id: params.userId }, body, {
       new: true,
       runValidators: true
     })
@@ -57,7 +45,7 @@ const userController = {
 
   // delete user
   deleteUser({ params }, res) {
-    User.findOneAndDelete({ _id: params.id })
+    User.findOneAndDelete({ _id: params.userId })
       .then(dbUserData => res.json(dbUserData))
       .catch(err => res.json(err));
   }
